b7a7a3e9d362f5ea1c3096e59f3369547a8e1e9f
CodeWasp <codewaspgithub@gmail.com>
2024-11-01 21:08:50 +0100

    Fixed box bumpers

--- src/gruppe1/pinballGame.js
+++ src/gruppe1/pinballGame.js
@@ -52,12 +52,12 @@ export function createBoard(textureObject, position, angle) {
 	let edge4Size = { width: 0.1, height: 0.3, depth: 5.5 };
 
 	let leader1Size = { width: 2.2, height: 0.3, depth: 0.1 };
-	let leader2Size = { width: 1.2, height: 0.3, depth: 0.1 };
-	let leader3Size = { width: 1.2, height: 0.3, depth: 0.1 };
-	let leader4Size = { width: 1.2, height: 0.3, depth: 0.1 };
-	let leader5Size = { width: 0.8, height: 0.3, depth: 0.1 };
-	let leader6Size = { width: 0.8, height: 0.3, depth: 0.1 };
-	let leader7Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	//let leader2Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	//let leader3Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	//let leader4Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	//let leader5Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	//let leader6Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	//let leader7Size = { width: 0.8, height: 0.3, depth: 0.1 };
 
 	let floorPosition = { x: 0.5, y: 0, z: 0 };
 	let floor2Position = { x: 0.5, y: 0.35, z: 0 };
@@ -68,12 +68,12 @@ export function createBoard(textureObject, position, angle) {
 	let bottomEdgePosition = { x: 0.5, y: 0.15, z: 3.7 };
 
 	let leader1Position = { x: 1.89, y: 0.15, z: -2.9 };
-	let leader2Position = { x: -0.8, y: 0.15, z: -2 };
-	let leader3Position = { x: -0.8, y: 0.15, z: -1.4 };
-	let leader4Position = { x: -0.8, y: 0.15, z: -0.4 };
-	let leader5Position = { x: -0.7, y: 0.15, z: 1.2 };
-	let leader6Position = { x: 0.8, y: 0.15, z: 0.4 };
-	let leader7Position = { x: 0.8, y: 0.15, z: -0.6 };
+	//let leader2Position = { x: -0.8, y: 0.15, z: -2 };
+	//let leader3Position = { x: -0.8, y: 0.15, z: -1.4 };
+	//let leader4Position = { x: -0.8, y: 0.15, z: -0.4 };
+	//let leader5Position = { x: -0.7, y: 0.15, z: 1.2 };
+	//let leader6Position = { x: 0.8, y: 0.15, z: 0.4 };
+	//let leader7Position = { x: 0.8, y: 0.15, z: -0.6 };
 
 	const floorMaterial = new THREE.MeshPhongMaterial({ color: 0xf78a1d });
 	const floor2Material = new THREE.MeshPhongMaterial({ color: 0xffffff, transparent: true, opacity: 0.25 });
@@ -157,46 +157,46 @@ export function createBoard(textureObject, position, angle) {
 	groupMesh.add(meshLeader1);
 
 	// Leader2:
-	let geoLeader2 = new THREE.BoxGeometry(leader2Size.width, leader2Size.height, leader2Size.depth);
-	let meshLeader2 = new THREE.Mesh(geoLeader2, edgeMaterial);
-	meshLeader2.position.set(leader2Position.x, leader2Position.y, leader2Position.z);
-	meshLeader2.rotateY(Math.PI/12);
-	groupMesh.add(meshLeader2);
+	//let geoLeader2 = new THREE.BoxGeometry(leader2Size.width, leader2Size.height, leader2Size.depth);
+	//let meshLeader2 = new THREE.Mesh(geoLeader2, edgeMaterial);
+	//meshLeader2.position.set(leader2Position.x, leader2Position.y, leader2Position.z);
+	//meshLeader2.rotateY(Math.PI/12);
+	//groupMesh.add(meshLeader2);
 
 	// Leader3:
-	let geoLeader3 = new THREE.BoxGeometry(leader3Size.width, leader3Size.height, leader3Size.depth);
-	let meshLeader3 = new THREE.Mesh(geoLeader3, edgeMaterial);
-	meshLeader3.position.set(leader3Position.x, leader3Position.y, leader3Position.z);
-	meshLeader3.rotateY(Math.PI/6);
-	groupMesh.add(meshLeader3);
+	//let geoLeader3 = new THREE.BoxGeometry(leader3Size.width, leader3Size.height, leader3Size.depth);
+	//let meshLeader3 = new THREE.Mesh(geoLeader3, edgeMaterial);
+	//meshLeader3.position.set(leader3Position.x, leader3Position.y, leader3Position.z);
+	//meshLeader3.rotateY(Math.PI/6);
+	//groupMesh.add(meshLeader3);
 
 	// Leader4:
-	let geoLeader4 = new THREE.BoxGeometry(leader4Size.width, leader4Size.height, leader4Size.depth);
-	let meshLeader4 = new THREE.Mesh(geoLeader4, edgeMaterial);
-	meshLeader4.position.set(leader4Position.x, leader4Position.y, leader4Position.z);
-	meshLeader4.rotateY(Math.PI/3);
-	groupMesh.add(meshLeader4);
+	//let geoLeader4 = new THREE.BoxGeometry(leader4Size.width, leader4Size.height, leader4Size.depth);
+	//let meshLeader4 = new THREE.Mesh(geoLeader4, edgeMaterial);
+	//meshLeader4.position.set(leader4Position.x, leader4Position.y, leader4Position.z);
+	//meshLeader4.rotateY(Math.PI/3);
+	//groupMesh.add(meshLeader4);
 
 	// Leader5:
-	let geoLeader5 = new THREE.BoxGeometry(leader5Size.width, leader5Size.height, leader5Size.depth);
-	let meshLeader5 = new THREE.Mesh(geoLeader5, edgeMaterial);
-	meshLeader5.position.set(leader5Position.x, leader5Position.y, leader5Position.z);
-	meshLeader5.rotateY(Math.PI/3);
-	groupMesh.add(meshLeader5);
+	//let geoLeader5 = new THREE.BoxGeometry(leader5Size.width, leader5Size.height, leader5Size.depth);
+	//let meshLeader5 = new THREE.Mesh(geoLeader5, edgeMaterial);
+	//meshLeader5.position.set(leader5Position.x, leader5Position.y, leader5Position.z);
+	//meshLeader5.rotateY(Math.PI/3);
+	//groupMesh.add(meshLeader5);
 
 	// Leader6:
-	let geoLeader6 = new THREE.BoxGeometry(leader6Size.width, leader6Size.height, leader6Size.depth);
-	let meshLeader6 = new THREE.Mesh(geoLeader6, edgeMaterial);
-	meshLeader6.position.set(leader6Position.x, leader6Position.y, leader6Position.z);
-	meshLeader6.rotateY(Math.PI/3);
-	groupMesh.add(meshLeader6);
+	//let geoLeader6 = new THREE.BoxGeometry(leader6Size.width, leader6Size.height, leader6Size.depth);
+	//let meshLeader6 = new THREE.Mesh(geoLeader6, edgeMaterial);
+	//meshLeader6.position.set(leader6Position.x, leader6Position.y, leader6Position.z);
+	//meshLeader6.rotateY(Math.PI/3);
+	//groupMesh.add(meshLeader6);
 
 	// Leader7:
-	let geoLeader7 = new THREE.BoxGeometry(leader7Size.width, leader7Size.height, leader7Size.depth);
-	let meshLeader7 = new THREE.Mesh(geoLeader7, edgeMaterial);
-	meshLeader7.position.set(leader7Position.x, leader7Position.y, leader7Position.z);
-	meshLeader7.rotateY(Math.PI+2.5);
-	groupMesh.add(meshLeader7);
+	//let geoLeader7 = new THREE.BoxGeometry(leader7Size.width, leader7Size.height, leader7Size.depth);
+	//let meshLeader7 = new THREE.Mesh(geoLeader7, edgeMaterial);
+	//meshLeader7.position.set(leader7Position.x, leader7Position.y, leader7Position.z);
+	//meshLeader7.rotateY(Math.PI+2.5);
+	//groupMesh.add(meshLeader7);
 
 	// AMMO:
 	let compoundShape = new Ammo.btCompoundShape();
@@ -210,12 +210,12 @@ export function createBoard(textureObject, position, angle) {
 	let bottomEdgeShape = new Ammo.btBoxShape(new Ammo.btVector3(edge3Size.width/2, edge3Size.height/2, edge3Size.depth/2));
 
 	let leader1Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader1Size.width/2, leader1Size.height/2, leader1Size.depth/2));
-	let leader2Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader2Size.width/2, leader2Size.height/2, leader2Size.depth/2));
-	let leader3Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader3Size.width/2, leader3Size.height/2, leader3Size.depth/2));
-	let leader4Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader4Size.width/2, leader4Size.height/2, leader4Size.depth/2));
-	let leader5Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader5Size.width/2, leader5Size.height/2, leader5Size.depth/2));
-	let leader6Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader6Size.width/2, leader6Size.height/2, leader6Size.depth/2));
-	let leader7Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader7Size.width/2, leader7Size.height/2, leader7Size.depth/2));
+	//let leader2Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader2Size.width/2, leader2Size.height/2, leader2Size.depth/2));
+	//let leader3Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader3Size.width/2, leader3Size.height/2, leader3Size.depth/2));
+	//let leader4Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader4Size.width/2, leader4Size.height/2, leader4Size.depth/2));
+	//let leader5Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader5Size.width/2, leader5Size.height/2, leader5Size.depth/2));
+	//let leader6Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader6Size.width/2, leader6Size.height/2, leader6Size.depth/2));
+	//let leader7Shape = new Ammo.btBoxShape(new Ammo.btVector3(leader7Size.width/2, leader7Size.height/2, leader7Size.depth/2));
 
 
 	let trans1Floor = new Ammo.btTransform();
@@ -260,47 +260,47 @@ export function createBoard(textureObject, position, angle) {
 	transLeader1.setRotation(new Ammo.btQuaternion(quaternion1.x, quaternion1.y, quaternion1.z, quaternion1.w));
 	compoundShape.addChildShape(transLeader1, leader1Shape);
 
-	let transLeader2 = new Ammo.btTransform();
-	transLeader2.setIdentity();
-	transLeader2.setOrigin(new Ammo.btVector3(leader2Position.x, leader2Position.y, leader2Position.z));
-	let quaternion2 = meshLeader2.quaternion;
-	transLeader2.setRotation(new Ammo.btQuaternion(quaternion2.x, quaternion2.y, quaternion2.z, quaternion2.w));
-	compoundShape.addChildShape(transLeader2, leader2Shape);
+	//let transLeader2 = new Ammo.btTransform();
+	//transLeader2.setIdentity();
+	//transLeader2.setOrigin(new Ammo.btVector3(leader2Position.x, leader2Position.y, leader2Position.z));
+	//let quaternion2 = meshLeader2.quaternion;
+	//transLeader2.setRotation(new Ammo.btQuaternion(quaternion2.x, quaternion2.y, quaternion2.z, quaternion2.w));
+	//compoundShape.addChildShape(transLeader2, leader2Shape);
 
-	let transLeader3 = new Ammo.btTransform();
-	transLeader3.setIdentity();
-	transLeader3.setOrigin(new Ammo.btVector3(leader3Position.x, leader3Position.y, leader3Position.z));
-	let quaternion3 = meshLeader3.quaternion;
-	transLeader3.setRotation(new Ammo.btQuaternion(quaternion3.x, quaternion3.y, quaternion3.z, quaternion3.w));
-	compoundShape.addChildShape(transLeader3, leader3Shape);
+	//let transLeader3 = new Ammo.btTransform();
+	//transLeader3.setIdentity();
+	//transLeader3.setOrigin(new Ammo.btVector3(leader3Position.x, leader3Position.y, leader3Position.z));
+	//let quaternion3 = meshLeader3.quaternion;
+	//transLeader3.setRotation(new Ammo.btQuaternion(quaternion3.x, quaternion3.y, quaternion3.z, quaternion3.w));
+	//compoundShape.addChildShape(transLeader3, leader3Shape);
 
-	let transLeader4 = new Ammo.btTransform();
-	transLeader4.setIdentity();
-	transLeader4.setOrigin(new Ammo.btVector3(leader4Position.x, leader4Position.y, leader4Position.z));
-	let quaternion4 = meshLeader4.quaternion;
-	transLeader4.setRotation(new Ammo.btQuaternion(quaternion4.x, quaternion4.y, quaternion4.z, quaternion4.w));
-	compoundShape.addChildShape(transLeader4, leader4Shape);
+	//let transLeader4 = new Ammo.btTransform();
+	//transLeader4.setIdentity();
+	//transLeader4.setOrigin(new Ammo.btVector3(leader4Position.x, leader4Position.y, leader4Position.z));
+	//let quaternion4 = meshLeader4.quaternion;
+	//transLeader4.setRotation(new Ammo.btQuaternion(quaternion4.x, quaternion4.y, quaternion4.z, quaternion4.w));
+	//compoundShape.addChildShape(transLeader4, leader4Shape);
 
-	let transLeader5 = new Ammo.btTransform();
-	transLeader5.setIdentity();
-	transLeader5.setOrigin(new Ammo.btVector3(leader5Position.x, leader5Position.y, leader5Position.z));
-	let quaternion5 = meshLeader5.quaternion;
-	transLeader5.setRotation(new Ammo.btQuaternion(quaternion5.x, quaternion5.y, quaternion5.z, quaternion5.w));
-	compoundShape.addChildShape(transLeader5, leader5Shape);
+	//let transLeader5 = new Ammo.btTransform();
+	//transLeader5.setIdentity();
+	//transLeader5.setOrigin(new Ammo.btVector3(leader5Position.x, leader5Position.y, leader5Position.z));
+	//let quaternion5 = meshLeader5.quaternion;
+	//transLeader5.setRotation(new Ammo.btQuaternion(quaternion5.x, quaternion5.y, quaternion5.z, quaternion5.w));
+	//compoundShape.addChildShape(transLeader5, leader5Shape);
 
-	let transLeader6 = new Ammo.btTransform();
-	transLeader6.setIdentity();
-	transLeader6.setOrigin(new Ammo.btVector3(leader6Position.x, leader6Position.y, leader6Position.z));
-	let quaternion6 = meshLeader6.quaternion;
-	transLeader6.setRotation(new Ammo.btQuaternion(quaternion6.x, quaternion6.y, quaternion6.z, quaternion6.w));
-	compoundShape.addChildShape(transLeader6, leader6Shape);
+	//let transLeader6 = new Ammo.btTransform();
+	//transLeader6.setIdentity();
+	//transLeader6.setOrigin(new Ammo.btVector3(leader6Position.x, leader6Position.y, leader6Position.z));
+	//let quaternion6 = meshLeader6.quaternion;
+	//transLeader6.setRotation(new Ammo.btQuaternion(quaternion6.x, quaternion6.y, quaternion6.z, quaternion6.w));
+	//compoundShape.addChildShape(transLeader6, leader6Shape);
 
-	let transLeader7 = new Ammo.btTransform();
-	transLeader7.setIdentity();
-	transLeader7.setOrigin(new Ammo.btVector3(leader7Position.x, leader7Position.y, leader7Position.z));
-	let quaternion7 = meshLeader7.quaternion;
-	transLeader7.setRotation(new Ammo.btQuaternion(quaternion7.x, quaternion7.y, quaternion7.z, quaternion7.w));
-	compoundShape.addChildShape(transLeader7, leader7Shape);
+	//let transLeader7 = new Ammo.btTransform();
+	//transLeader7.setIdentity();
+	//transLeader7.setOrigin(new Ammo.btVector3(leader7Position.x, leader7Position.y, leader7Position.z));
+	//let quaternion7 = meshLeader7.quaternion;
+	//transLeader7.setRotation(new Ammo.btQuaternion(quaternion7.x, quaternion7.y, quaternion7.z, quaternion7.w));
+	//compoundShape.addChildShape(transLeader7, leader7Shape);
 
 	//compoundShape.setMargin(0.05);
 
@@ -318,6 +318,7 @@ export function createBoard(textureObject, position, angle) {
  * Dette henger sammen med kollisjonshåndteringen. Se myAmmoHelper.js og checkCollisions-funksjonen.
  */
 function addBumpers(angle) {
+	// Cylinder bumpers
 	let bumper1Size = {radiusTop:0.2, radiusBottom: 0.2, height: 0.4};
 	let bumper1Position = {x: 0, y: bumper1Size.height/2, z:0};
 	bumper1Position.y += (-Math.tan(angle) * bumper1Position.z);
@@ -348,14 +349,50 @@ function addBumpers(angle) {
 	bumper6Position.y += (-Math.tan(angle) * bumper6Position.z);
 	addBumper(angle, bumper6Size, bumper6Position, "bumper6", 200);
 
+	// Box bumpers
+	let bumper7Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	let bumper7Position = { x: -0.8, y: 0.15, z: -2 };
+	bumper7Position.y += (-Math.tan(angle) * bumper7Position.z);
+	addBumper(angle, bumper7Size, bumper7Position, "bumper7", 200, "box", Math.PI/12);
+
+	let bumper8Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	let bumper8Position = { x: -0.8, y: 0.15, z: -1.4 };
+	bumper8Position.y += (-Math.tan(angle) * bumper8Position.z);
+	addBumper(angle, bumper8Size, bumper8Position, "bumper8", 200, "box", Math.PI/6);
+
+	let bumper9Size = { width: 1.2, height: 0.3, depth: 0.1 };
+	let bumper9Position = { x: -0.8, y: 0.15, z: -0.4 };
+	bumper9Position.y += (-Math.tan(angle) * bumper9Position.z);
+	addBumper(angle, bumper9Size, bumper9Position, "bumper9", 200, "box", Math.PI/3);
+
+	let bumper10Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	let bumper10Position = { x: -0.7, y: 0.15, z: 1.2 };
+	bumper10Position.y += (-Math.tan(angle) * bumper10Position.z);
+	addBumper(angle, bumper10Size, bumper10Position, "bumper10", 200, "box", Math.PI/3);
+
+	let bumper11Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	let bumper11Position = { x: 0.8, y: 0.15, z: 0.4 };
+	bumper11Position.y += (-Math.tan(angle) * bumper11Position.z);
+	addBumper(angle, bumper11Size, bumper11Position, "bumper11", 200, "box", Math.PI+2.5);
+
+	let bumper12Size = { width: 0.8, height: 0.3, depth: 0.1 };
+	let bumper12Position = { x: 0.8, y: 0.15, z: -0.6 };
+	bumper12Position.y += (-Math.tan(angle) * bumper12Position.z);
+	addBumper(angle, bumper12Size, bumper12Position, "bumper12", 200, "box", Math.PI/3);
 }
 
 /**
  * Legger til en bumper. Bumperen er en sylinder med radiusTop, radiusBottom og height.
  */
-function addBumper(angle, size, position, name, points) {
+function addBumper(angle, size, position, name, points, type = "cylinder", angleY = 0) {
 	const material = new THREE.MeshPhongMaterial({color: 0x0ef21a, transparent: false});
-	let geoBumper = new THREE.CylinderGeometry(size.radiusTop, size.radiusBottom, size.height, size.width, size.radialSegments, size.openEnded, size.thetaStart, size.thetaLength);
+	let geoBumper
+	if (type == "cylinder") {
+		geoBumper = new THREE.CylinderGeometry(size.radiusTop, size.radiusBottom, size.height, size.width, size.radialSegments, size.openEnded, size.thetaStart, size.thetaLength);
+	}
+	if (type == "box") {
+		geoBumper = new THREE.BoxGeometry(size.width, size.height, size.depth);
+	}
 	let meshBumper = new THREE.Mesh(geoBumper, material);
 	meshBumper.name =  name;
 	meshBumper.points = points;
@@ -365,11 +402,20 @@ function addBumper(angle, size, position, name, points) {
 		mesh1.material.color.setHex(Math.random() * 0xffffff);
 	};
 	meshBumper.position.set(position.x, position.y, position.z);
+	if (type == "box") {
+		meshBumper.rotateY(angleY);
+	}
 	meshBumper.rotation.x = angle;
 
 	addMeshToScene(meshBumper);
 	let mass = 0
-	let bumperShape = new Ammo.btCylinderShape(new Ammo.btVector3(size.radiusTop, size.radiusBottom, size.height, size.width, size.radialSegments, size.openEnded, size.thetaStart, size.thetaLength));
+	let bumperShape;
+	if (type == "cylinder") {
+		bumperShape = new Ammo.btCylinderShape(new Ammo.btVector3(size.radiusTop, size.radiusBottom, size.height, size.width, size.radialSegments, size.openEnded, size.thetaStart, size.thetaLength));
+	}
+	if (type == "box") {
+		bumperShape = new Ammo.btBoxShape(new Ammo.btVector3(size.width/2, size.height/2, size.depth/2));
+	}
 	// Sørg for "fysikk" på denne.
 	// Tips: Bruk createAmmoRigidBody(...)
 	let rigidBody = createAmmoRigidBody(bumperShape, meshBumper, 0.2, 0.9, position, mass);
